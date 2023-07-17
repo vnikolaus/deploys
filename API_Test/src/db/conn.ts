c7d7d800b916d2d58623eecac012006e3cab5642
vnikolaus <victor_nikolaus@hotmail.com>
2023-07-16 22:29:52 -0300

    commit deploys

--- API_Test/src/db/conn.ts
+++ API_Test/src/db/conn.ts
@@ -3,12 +3,8 @@ import { config } from 'dotenv'
 config()
 
 export const conn = createConnection({
-    // host: process.env.MYSQL_HOST,
-    // user: process.env.MYSQL_USER,
-    // password: process.env.MYSQL_PWD,
-    // database: process.env.MYSQL_DATABASE,
-    host: 'localhost',
-    user: 'nk',
-    password: 'nikol@us06',
-    database: 'deployjs',
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PWD,
+    database: process.env.MYSQL_DATABASE,
 }).promise()
